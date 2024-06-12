6cb53806f2ce6c29a5ef165eaf974c5271cb87a8
Sevara <sevararamanova57@gmail.com>
2024-06-13 04:18:51 +0500

    fayllar qo'shildi

---
+++ src/App.jsx
@@ -0,0 +1,305 @@
+
+import './App.css';
+import logo from './images/LogoColor.png';//1
+import avatar from './images/Avatar.png';//2
+import icon from './images/Icon 01.png';//3
+import icon2 from './images/IconColor.png';//4
+import icon1 from './images/Icon.png';//5
+import avatar2 from './images/Image.png';//6
+import soat from './images/19px.png';//7
+import avatar_img from './images/Avatar_img.png';//8
+import camera_img from './images/12px.png';//9
+import telefon from './images/14px.png';//10
+import avatar_img2 from './images/Image_imag5.png';//11
+import telefon2 from './images/13px.png';//12
+import avatar_img4 from './images/Image_imag4.png';//13
+import telefon3 from './images/11px.png';//14
+import avatar_img5 from './images/Image_imag3.png';//15
+import telefon4 from './images/10px.png';//16
+import avatar_img6 from './images/Image_imag2.png';//17
+import sohil from './images/22px.png';//18
+import bozor from './images/17px.png';//19
+import car from './images/15px-1.png';//20
+import saxro from './images/saxro.png';//21
+import soxil from './images/16px.png';//22
+import iconca from './images/Iconoq.png';//23
+import next from './images/next_icon.png';//24
+import face from './images/Social 01.png';//25
+import insta from './images/Social 02.png';//26
+import twit from './images/Social 03.png';//27
+import be from './images/Social 04.png';//28
+
+
+function App() {
+  return (
+      <div className="container">
+        <section>
+          <div className="logo">
+               <img src={logo} alt="" />
+             <div className="logo_tittle">
+               <img src={avatar} alt="" />
+            <span>Bruce Lee</span>
+              <img src={icon1} alt="" />
+              <img src={icon} alt="" />
+              <img src={icon2} alt="" />
+             </div>
+          </div>
+          <div className="hero">
+            <div className="hero_tittle">
+              <h2>
+                5 Mind blowing facts about the Iphone 14
+              </h2>
+             <div className='hero_tittle_author'>
+             <img src={avatar2} alt="" />
+              <p>Farok Rastegar  .  1 hour ago</p>
+             </div>
+            </div>
+          </div>
+          </section>
+        <section>
+          <div className="wrapper">
+            <h2>Featured articles</h2>
+          <div>
+          <div className="wrapper__tittle">
+              <div className="wrapper__tittle_text">
+                <img src={soat} alt="" />
+                <h4>10 beaches you must visit</h4>
+                <div className='wrapper_text'>
+                  <img src={avatar_img} alt="" />
+                  <p>Farok Rastegar  .  1 hour ago</p>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the…</p>
+                </div>
+              </div>
+            </div>
+            <div className="wrapper__tittle">
+              <div className="wrapper__tittle_text">
+                <img src={camera_img} alt="" />
+                <h4>10 beaches you must visit</h4>
+                <div className='wrapper_text'>
+                  <img src={avatar2} alt="" />
+                  <p>Farok Rastegar  .  1 hour ago</p>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the…</p>
+                </div>
+              </div>
+            </div>
+            <div className="wrapper__tittle">
+              <div className="wrapper__tittle_text">
+                <img src={telefon} alt="" />
+                <h4>10 beaches you must visit</h4>
+                <div className='wrapper_text'>
+                  <img src={avatar_img2} alt="" />
+                  <p>Farok Rastegar  .  1 hour ago</p>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the…</p>
+                </div>
+              </div>
+            </div>
+            <div className="wrapper__tittle">
+              <div className="wrapper__tittle_text">
+                <img src={telefon2} alt="" />
+                <h4>10 beaches you must visit</h4>
+                <div className='wrapper_text'>
+                  <img src={avatar_img4} alt="" />
+                  <p>Farok Rastegar  .  1 hour ago</p>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the…</p>
+                </div>
+              </div>
+            </div>
+            <div className="wrapper__tittle">
+              <div className="wrapper__tittle_text">
+                <img src={telefon3} alt="" />
+                <h4>10 beaches you must visit</h4>
+                <div className='wrapper_text'>
+                  <img src={avatar_img5} alt="" />
+                  <p>Farok Rastegar  .  1 hour ago</p>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the…</p>
+                </div>
+              </div>
+            </div>
+            <div className="wrapper__tittle">
+              <div className="wrapper__tittle_text">
+                <img src={telefon4} alt="" />
+                <h4>10 beaches you must visit</h4>
+                <div className='wrapper_text'>
+                  <img src={avatar_img6} alt="" />
+                  <p>Farok Rastegar  .  1 hour ago</p>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the…</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+          <div className="featured">
+            <div className="featured__tittle">
+              <button>Featured</button>
+              <h2>
+              Beached to take you somewhere else
+              </h2>
+                <div className="featured__tittle_img">
+                  <img className='featured__tittle_imag' src={avatar2} alt="" />
+                  <p>Farok Rastegar  .  1 hour ago</p>
+                </div>
+            </div>
+          </div>
+          <div className="pages">
+          <h2>Featured articles</h2>
+            <div className='pages_tittle'>
+              <div className="pages_tittle_img">
+                <img src={sohil} alt=""/>
+                <div className='pages_tittle_img_text'>
+                  <h2>
+                  Pathway to the Mediterranean
+                  </h2>
+                 <div>
+                 <img src={avatar2} alt="" />
+                  <p>Farok Rastegar  .  1 hour ago</p>
+                 </div>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain a…</p>
+                </div>
+              </div>
+              <div className="pages_tittle_img">
+                <img src={bozor} alt=""/>
+                <div className='pages_tittle_img_text'>
+                  <h2>
+                  Pathway to the Mediterranean
+                  </h2>
+                 <div>
+                 <img src={avatar2} alt="" />
+                  <p>Farok Rastegar  .  1 hour ago</p>
+                 </div>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain a…</p>
+                </div>
+              </div>
+              <div className="pages_tittle_img">
+                <img src={car} alt=""/>
+                <div className='pages_tittle_img_text'>
+                  <h2>
+                  Pathway to the Mediterranean
+                  </h2>
+                 <div>
+                 <img src={avatar2} alt="" />
+                  <p>Farok Rastegar  .  1 hour ago</p>
+                 </div>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain a…</p>
+                </div>
+              </div>
+              <div className="pages_tittle_img">
+                <img src={saxro} alt=""/>
+                <div className='pages_tittle_img_text'>
+                  <h2>
+                  Pathway to the Mediterranean
+                  </h2>
+                 <div>
+                 <img src={avatar2} alt="" />
+                  <p>Farok Rastegar  .  1 hour ago</p>
+                 </div>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain a…</p>
+                </div>
+              </div>
+              <div className="pages_tittle_img">
+                <img src={soxil} alt=""/>
+                <div className='pages_tittle_img_text'>
+                  <h2>
+                  Pathway to the Mediterranean
+                  </h2>
+                 <div>
+                 <img src={avatar2} alt="" />
+                  <p>Farok Rastegar  .  1 hour ago</p>
+                 </div>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain a…</p>
+                </div>
+              </div>
+
+              <div className='pages_button'>
+               <a href="#"> <button>Load more</button></a>
+              </div>
+
+
+
+            </div>
+            
+            
+
+          </div>
+          <div className="layout">
+            <div>
+              <h2 className='layout__text'>
+              Incredible deals, <br /> right to your inbox!
+              </h2>
+             
+             <div className='layout_icon'>
+             <input className='layout__inp' placeholder='Placeholder' type="text" /> 
+              <div className='icon_tittle'>
+               <img className='layout_icons' src={iconca} alt="" />
+             </div>
+             </div>
+              <p className='layout_text'>
+              By joining you agree to our Terms and Conditions
+              </p>
+            </div>
+          </div>
+          <div className='futer'>
+            <div className='futer_logo'>
+            <ul className='futer_ul'>
+              <li className='futer_ul_li'>First Column</li>
+              <li className='futer_ul__li'>Link One</li>
+              <li className='futer_ul__li'>Link Two</li>
+              <li className='futer_ul__li'>Link Three</li>
+              <li className='futer_ul__li'>Link Four</li>
+            </ul>
+            <ul className='futer_ul'>
+              <li className='futer_ul_li'>First Column</li>
+              <li className='futer_ul__li'>Link One</li>
+              <li className='futer_ul__li'>Link Two</li>
+              <li className='futer_ul__li'>Link Three</li>
+              <li className='futer_ul__li'>Link Four</li>
+            </ul>
+            <ul className='futer_ul'>
+              <li className='futer_ul_li'>First Column</li>
+              <li className='futer_ul__li'>Link One</li>
+              <li className='futer_ul__li'>Link Two</li>
+              <li className='futer_ul__li'>Link Three</li>
+              <li className='futer_ul__li'>Link Four</li>
+            </ul>
+             <ul className='futer_ul'>
+              <li className='futer_ul_li'>First Column</li>
+              <li className='futer_ul__li'>Link One</li>
+              <li className='futer_ul__li'>Link Two</li>
+              <li className='futer_ul__li'>Link Three</li>
+              <li className='futer_ul__li'>Link Four</li>
+            </ul>
+              <div>
+                <p>
+                Never miss an update
+                </p>
+               <div className='futer_tittle_icon'>
+               <input className='futer_input'   placeholder='placeholder' type="text" />
+                <div className='futer_tittle'>
+                    <img className='layout_icons' src={iconca} alt="" />
+               </div>
+             </div>
+              </div>
+            
+            </div>
+
+          </div>
+          <div className='next'>
+            <img src={next} alt="" />
+            <p>© 2019 Fabrx </p>
+            <div className='futer_icon__next'>
+              <img src={face} alt="" />
+              <img src={insta} alt="" />
+              <img src={twit} alt="" />
+              <img src={be} alt="" />
+       
+            </div>
+          </div>
+        </section>
+
+
+      </div>
+
+   
+  );
+}
+
+export default App;
